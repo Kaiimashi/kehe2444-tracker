03a4938d718c25f657159c13c2ffee22e7e939df
Kevin He <kh181202@gmail.com>
2024-06-06 00:03:54 +1000

    Responsiveness Complete
    Web nav now responsive for desktop and mobile. On desktop, users access tabs using the right side navbar. On mobile, users access tabs by clicking on a burger menu icon to reveal a navbar containing tabs. Users can then click the "X" to hide the navbar.

---
+++ public/script.js
@@ -0,0 +1,10 @@
+//displays/hides mobile menu when user clicks the burger menu icon/cross 
+function showMobileMenu() {
+    const mobilemenu = document.querySelector ('.mobilemenu') 
+    mobilemenu.style.display = 'flex';
+}
+
+function hideMobileMenu() {
+    const mobilemenu = document.querySelector ('.mobilemenu') 
+    mobilemenu.style.display = 'none';
+}
