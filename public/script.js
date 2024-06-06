0327893a32c215b8bf68c47c3873eba7ee97b5cd
Kevin He <kh181202@gmail.com>
2024-06-06 15:20:59 +1000

    Recipe Time Tracker Styling Complete
    Finished the styling by improving the 'add new recipe' button before moving on to JS code for the time tracker.

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-//displays/hides mobile menu when user clicks the burger menu icon/cross 
+// displays/hides mobile menu when user clicks the burger menu icon/cross 
 function showMobileMenu() {
     const mobilemenu = document.querySelector ('.mobilemenu') 
     mobilemenu.style.display = 'flex';
@@ -8,3 +8,5 @@ function hideMobileMenu() {
     const mobilemenu = document.querySelector ('.mobilemenu') 
     mobilemenu.style.display = 'none';
 }
+
+// Recipe Time Tracker JS
